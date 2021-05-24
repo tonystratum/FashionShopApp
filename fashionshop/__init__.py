b7c2f530e3669de6f3fa3a493f49424866300cd4
Anton Kantsemal <a.kantsemal@mobidev.biz>
2021-05-24 19:51:35 +0300

    add flask-admin

--- fashionshop/__init__.py
+++ fashionshop/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from datetime import timedelta
 from elasticsearch import Elasticsearch
 import os
@@ -16,4 +18,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+admin = Admin(app)
+from fashionshop.models import *
 from fashionshop import routes
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Product, db.session))
+admin.add_view(ModelView(CartItem, db.session))
