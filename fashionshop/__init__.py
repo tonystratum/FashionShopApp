8416c08fa03c806d76d76623698ab628ff4e81d6
Anton Kantsemal <a.kantsemal@mobidev.biz>
2021-05-25 09:16:23 +0300

    move to postgres

--- fashionshop/__init__.py
+++ fashionshop/__init__.py
@@ -8,10 +8,16 @@ from datetime import timedelta
 from elasticsearch import Elasticsearch
 import os
 
+# postgresql cfg
+USER = "postgres"
+PASSWORD = "postgres"
+HOST = "localhost"
+DB_NAME = "fsapp"
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.permanent_session_lifetime = timedelta(minutes = 20)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DB_NAME}'
 es = Elasticsearch()
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -22,6 +28,10 @@ admin = Admin(app)
 from fashionshop.models import *
 from fashionshop import routes
 
+db.create_all()
+db.session.commit()
+
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Product, db.session))
 admin.add_view(ModelView(CartItem, db.session))
+admin.add_view(ModelView(Category, db.session))
--- utils.py
+++ utils.py
@@ -1,23 +1,53 @@
 from PIL import Image
 import os
 
-basepath = 'fashionshop\static\img\product\Fragrance'
+basepath = os.path.abspath("fashionshop/static/img/product/Fragrance")
+
+
 # os.chdir(basepath)
 def save_img(img):
-    i = Image.open(os.path.join(basepath,img))
+    i = Image.open(os.path.join(basepath, img))
     t, f_ext = os.path.splitext(i.filename)
-    text = t.replace("-"," ")
+    text = t.replace("-", " ")
     f = text + f_ext
-    print('infor:',img, i.format, i.size, i.mode)
+    print('infor:', img, i.format, i.size, i.mode)
     if i.mode == 'RGBA':
         i = i.convert('RGB')
-    output = (264,363)
+    output = (264, 363)
     i.thumbnail(output, Image.ANTIALIAS)
     # i = i.resize(output, Image.ANTIALIAS)
     i.save(f, "JPEG")
-    print('infor changed:',img, i.format, i.size, i.mode)
+    print('infor changed:', img, i.format, i.size, i.mode)
     pass
+
 
 for img in os.listdir(basepath):
     save_img(img)
-    
+
+
+def resultproxy_to_dict(resultproxy):
+    """
+    Converts SQLAlchemy.engine.result.ResultProxy to a list of rows,
+    represented as dicts.
+    :param resultproxy: SQLAlchemy.engine.result.ResultProxy
+    :return: rows as dicts.
+    """
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    return a
+
+
+def query_to_dict(db, query):
+    """
+    Wrapper for raw SQL to dict execution.
+    :param db: db object from fashionshop.
+    :param query: raw SQL string.
+    :return: rows as dicts.
+    """
+    return resultproxy_to_dict(db.engine.execute(query))
